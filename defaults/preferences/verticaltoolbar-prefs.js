61dc3743a703ec339ac1f95b40703d7e3f53e467
Gomita <gomita@xuldev.org>
2011-01-13 18:58:37 +0900

    animate and sidesync pref are now enabled by default

--- defaults/preferences/verticaltoolbar-prefs.js
+++ defaults/preferences/verticaltoolbar-prefs.js
@@ -1,5 +1,5 @@
 pref("extensions.verticaltoolbar.direction", "");
 pref("extensions.verticaltoolbar.display", 1);
 pref("extensions.verticaltoolbar.display.fullscreen", 2);
-pref("extensions.verticaltoolbar.animate", false);
-pref("extensions.verticaltoolbar.sidesync", false);
+pref("extensions.verticaltoolbar.animate", true);
+pref("extensions.verticaltoolbar.sidesync", true);
